c6e170c55b1675cb506a3ba6398215d8883a7b0a
marisapetrusky <marisapetrusky@gmail.com>
2021-03-17 02:16:27 -0400

    mode 1

--- input/L30_35-40_055.py
+++ input/L30_35-40_055.py
@@ -5,8 +5,8 @@ simulation_name = 'QUASI3D'
 shape = 'rectangle'
 # dt = 0.005, 150000
 iterations = 500000
-mode = -1
-fname = "L30_35-40_05.npz"
+mode = 1
+fname = "L30_35-40_05_M1.npz"
 
 # Probe centered at the following initial coordinates (in c/w_p):
 x_c = -2.4 # Start within region of field
