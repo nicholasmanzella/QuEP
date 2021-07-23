New weighting - fix high density problem
This update will fix memory issues with large simulations by generating weights one x-slice at a time. `movieWriter.py` is also now included to create the movie from frames produced using `OpenCV` library.

include/plotWeights.py
@@ -2,6 +2,7 @@
 
 import math
 import scipy.stats as stats
+import scipy.integrate as integrate
 import numpy as np
 import matplotlib.colors as col
 import matplotlib.pyplot as plt
@@ -12,10 +13,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import pdb
 mpl.use('Agg')
 
-def plotx(w_virt,xv,beamx_c,beamy_c,sigma_x,sigma_y):
+def plotx(w_virt,xv,yv,beamx_c,beamy_c,sigma_x,sigma_y):
 # 2D: w_x vs. x
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
+
+    middle_x = len(w_virt[:,0,0]) // 2 - 1
+    middle_y = len(w_virt[0,:,0]) // 2 - 1
+    middle_xi = len(w_virt[0,0,:]) // 2 - 1
     
     mu_x = beamx_c
     sigma_x = sigma_x
@@ -23,52 +28,64 @@ def plotx(w_virt,xv,beamx_c,beamy_c,sigma_x,sigma_y):
     sigma_y = sigma_y
     x = np.linspace(mu_x - 3*sigma_x, mu_x + 3*sigma_x, 100)
     y = np.linspace(mu_y - 3*sigma_y, mu_y + 3*sigma_y, 100)
-    #normalpdf = 1/(2*np.pi*sigma_x*sigma_y)*
-    normalpdf = np.exp((-1/2)*(((x-mu_x)/sigma_x)**2+((y-mu_y)/sigma_y)**2))
+    #multivariate_normalpdf = np.exp((-1/2)*(((x-mu_x)/sigma_x)**2+((y-mu_y)/sigma_y)**2))
+    #ax2.plot(y, multivariate_normalpdf, label="mulitvariate normal distribution")
+
+    
+    normalpdf = (1/(np.sqrt(2*np.pi)*sigma_y)) * (1/(np.sqrt(2*np.pi)*sigma_x)) * (np.exp((-1/2)*((x-mu_x)/sigma_x)**2)) * (np.exp((-1/2)*((yv[middle_x,middle_y,middle_xi]-mu_y)/sigma_y)**2))
     ax1.plot(x, normalpdf, label="normal distribution")
 
-    ax1.plot(xv[:,0,0],w_virt[:,0,0],"o", label="weighting_function")
+    normalpdf_func = lambda x: (1/(np.sqrt(2*np.pi)*sigma_y)) * (1/(np.sqrt(2*np.pi)*sigma_x)) * (np.exp((-1/2)*((x-mu_x)/sigma_x)**2)) * (np.exp((-1/2)*((yv[middle_x,middle_y,middle_xi]-mu_y)/sigma_y)**2))
+    area = integrate.quad(normalpdf_func,mu_x - 3*sigma_x, mu_x + 3*sigma_x)[0]
+
+    ax1.text(beamx_c,0.5,f"Area under curve = {'%.3f' % area}", fontdict=None, horizontalalignment='center', fontsize=10)
+    
+    ax1.plot(xv[:,middle_y,middle_xi],w_virt[:,middle_y,middle_xi],"o", label="weighting_function",alpha=0.7)
     
     ax1.legend()
     ax1.set_xlabel("x_0 ($c/\omega_p$)")
     ax1.set_ylabel("w_virt")
     ax1.set_title("Weighting viewing x-direction")
 
+    plt.tight_layout()
+
     fig1.savefig('weights_x-direction.png',dpi=600,transparent=False)
 
-def plotxlayer(layerx,weight,beamx_c,sigma_x):
-    fig3 = plt.figure()
-    ax3 = fig3.add_subplot(111)
-    
-    mu = beamx_c
-    sigma = sigma_x
-    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-    ax3.plot(x, math.sqrt(2*math.pi)*sigma_x*stats.norm.pdf(x, mu, sigma), label="normal distribution")
-
-    ax3.plot(layerx,weight,"o", label="weighting_function")
-    
-    ax3.legend()
-    ax3.set_xlabel("layer x position ($c/\omega_p$)")
-    ax3.set_ylabel("weight")
-    ax3.set_title("Weighting in x-direction of each x layer")
-
-    fig3.savefig('weights_x-direction-layers.png',dpi=600,transparent=False)
-
-def ploty(w_virt,yv,beamy_c,sigma_y):
+def ploty(w_virt,xv,yv,beamx_c,beamy_c,sigma_x,sigma_y):
 # 2D: w_y vs. y
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111)
     
-    mu = beamy_c
-    sigma = sigma_y
-    x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
-    ax2.plot(x, math.sqrt(2*math.pi)*sigma_y*stats.norm.pdf(x, mu, sigma), label="normal distribution")
+    middle_x = len(w_virt[:,0,0]) // 2 - 1
+    middle_y = len(w_virt[0,:,0]) // 2 - 1
+    middle_xi = len(w_virt[0,0,:]) // 2 - 1
 
-    ax2.plot(yv[0,:,0],w_virt[0,:,0],"o", label="weighting_function")
+    mu_x = beamx_c
+    sigma_x = sigma_x
+    mu_y = beamy_c
+    sigma_y = sigma_y
+    x = np.linspace(mu_x - 3*sigma_x, mu_x + 3*sigma_x, 100)
+    y = np.linspace(mu_y - 3*sigma_y, mu_y + 3*sigma_y, 100)
+    #multivariate_normalpdf = np.exp((-1/2)*(((x-mu_x)/sigma_x)**2+((y-mu_y)/sigma_y)**2))
+    #ax2.plot(y, multivariate_normalpdf, label="mulitvariate normal distribution")
+
+    
+    normalpdf = (1/(np.sqrt(2*np.pi)*sigma_y)) * (1/(np.sqrt(2*np.pi)*sigma_x)) * (np.exp((-1/2)*((x-mu_x)/sigma_x)**2)) * (np.exp((-1/2)*((yv[middle_x,middle_y,middle_xi]-mu_y)/sigma_y)**2))
+    ax2.plot(y, normalpdf, label="normal distribution")
+
+    normalpdf_func = lambda y: (1/(np.sqrt(2*np.pi)*sigma_y)) * (1/(np.sqrt(2*np.pi)*sigma_x)) * (np.exp((-1/2)*((xv[middle_x,middle_y,middle_xi]-mu_x)/sigma_x)**2)) * (np.exp((-1/2)*((y-mu_y)/sigma_y)**2))
+    area = integrate.quad(normalpdf_func,mu_y - 3*sigma_y, mu_y + 3*sigma_y)[0]
+
+    ax2.text(0.0,0.5,f"Area under curve = {'%.3f' % area}", fontdict=None, horizontalalignment='center', fontsize=10)
+     
+    ax2.plot(yv[middle_x,:,middle_xi],w_virt[middle_x,:,middle_xi],"o", label="weighting_function", alpha=0.3)
     
     ax2.legend()
     ax2.set_xlabel("y_0 ($c/\omega_p$)")
     ax2.set_ylabel("w_virt")
     ax2.set_title("Weighting viewing y-direction")
 
+    plt.tight_layout()
+
     fig2.savefig('weights_y-direction.png',dpi=600,transparent=False)
+

include/weighting_masks_function.py
@@ -2,6 +2,7 @@ import sys
 import math
 import numpy as np
 import pdb
+import progressbar
 
 # Creates weights based on distribution and inputted masks (below)
 
@@ -15,7 +16,7 @@ def getWeights(beamx_c,beamy_c,beamxi_c,x_c,y_c,xi_c,s1,s2,xdensity,ydensity,xid
 
 # Calculate s3 
     s3 = xstep*(xdensity-1)/2.0
-    print(f"s3 = {s3}")
+    print(f"s3 = {s3}\n")
     
 # Define corners (front is first to enter field)
     ytop = y_c + s1
@@ -31,57 +32,74 @@ def getWeights(beamx_c,beamy_c,beamxi_c,x_c,y_c,xi_c,s1,s2,xdensity,ydensity,xid
     xn = xfront
     zn = xiright + t0
     
+    print("Creating weighting arrays...")
 # Create individual coordiante arrays    
     x_0 = np.linspace(xfront,xback,xdensity)
     y_0 = np.linspace(ytop,ybot,ydensity)
     xi_0 = np.linspace(xiright,xileft,xidensity)
 
-# Create 3d virtual coordinate arrays 
-    xv, yv, xiv = np.meshgrid(x_0, y_0, xi_0,indexing='ij')
-
-# Create 3d virtual weight arrays containing weight of each particle
-    w_x = np.exp((-1.*(xv-beamx_c)**2)/(2*sigma_x**2))
+# Create empty weighting list
+    w=[]
+    w = [0 for k in range(0,noObj)]
+    yv = y_0.reshape(1,ydensity,1)
     w_y = np.exp((-1.*(yv-beamy_c)**2)/(2*sigma_y**2))
 
-# Weighting options evaluator
-    if (useWeights_x) and (useWeights_y):
-        w_virt = w_x * w_y
-    elif (useWeights_x):
-        w_virt = w_x
-    elif (useWeights_y):
-        w_virt = w_y
+# Loop through x layers to calculate weights with masks and add to 2D projection
+    for i in progressbar.progressbar(range(0,len(x_0)), redirect_stout=True):
+        # Create 3d virtual coordinate array for x-slice
+        xv, yv, xiv = np.meshgrid(x_0[i], y_0, xi_0,indexing='ij',sparse=True)
+        
+        # Create 3d virtual weight arrays containing weight of each particle in x-slice
+        w_x = np.exp((-1.*(x_0[i]-beamx_c)**2)/(2*sigma_x**2))
+        
+        # Weighting options evaluator
+        if (useWeights_x) and (useWeights_y):
+            w_virt = w_x * w_y
+        elif (useWeights_x):
+            w_virt = w_x
+        elif (useWeights_y):
+            w_virt = w_y
 
-# MASKING 
-    if (useMasks_xi):
-        #Define masks in z direction, leftmost z-coordinate = 0. Change if different mask is desired
-        left_of_masks= [30,43]  # left most limit of each mask in order
-        right_of_masks = [32,48]  # right most limit of each mask in order
+         # MASKING
+        if (not (useMasks_y)) and (not (useMasks_xi)):
+            w_virt = np.where(xiv == None, 0, w_virt)
+        
+        if (useMasks_xi):
+            # Define masks in z direction, leftmost z-coordinate = 0. Change if different mask is desired
+            left_of_masks= [-12]  # left most limit of each mask in order
+            right_of_masks = [-9]  # right most limit of each mask in order
 
-        print(zn)
-        # Conversion of z-coord to xi-coord
-        for i in range(0,len(left_of_masks)):
-            left_of_masks[i] = left_of_masks[i] - zn
-            right_of_masks[i] = right_of_masks[i] - zn
+            # Conversion of z-coord to xi-coord
+            for m in range(0,len(left_of_masks)):
+                left_of_masks[m] = left_of_masks[m] - zn
+                right_of_masks[m] = right_of_masks[m] - zn
 
-        for g in range(0,len(left_of_masks)):
-            w_virt = np.where(np.logical_and(xiv > left_of_masks[g], xiv < right_of_masks[g]), 0, w_virt)
+            # Apply masks to w_virt
+            for g in range(0,len(left_of_masks)):
+                w_virt = np.where(np.logical_and(xiv > left_of_masks[g], xiv < right_of_masks[g]), 0, w_virt)
+                
+        if (useMasks_y):
+            # Define masks in y direction, 0 is 0 on the y-axis. Change if different mask is desired
+            top_of_masks = [-0.3,0.8]  #upper limit of each mask in order
+            bot_of_masks = [-0.4,0.5]  #lower limit of each mask in order 
 
-    if (useMasks_y):
-        #Define masks in y direction, 0 is 0 on the y-axis. Change if different mask is desired
-        top_of_masks = [-0.3,0.8]  #upper limit of each mask in order
-        bot_of_masks = [-0.4,0.5]  #lower limit of each mask in order 
-
-        for h in range(0,len(top_of_masks)):
-            w_virt = np.where(np.logical_and(yv > bot_of_masks[h], yv < top_of_masks[h]), 0, w_virt)
-# END OF MASKING
-
-# Create final weighting list to return
-# Maps 3d virtual particles onto 2d projection
-    w = []
-    w = [0 for k in range(0,noObj)]
-    for i in range(0,xdensity):
+            # Apply masks to w_virt
+            w_virt = np.where(xiv == None, 0, w_virt)
+            for h in range(0,len(top_of_masks)):
+                w_virt = np.where(np.logical_and(yv > bot_of_masks[h], yv < top_of_masks[h]), 0, w_virt)
+        # END OF MASKING    
+            
+        # Create final weighting list w to return
+        # Maps 3d virtual particles in x-layer onto 2d projection appropriate location
         for j in range(0,ydensity):
             for k in range(0,xidensity):
-                w[xidensity_ * j + k + i] += w_virt[i,j,k]
+                w[xidensity_ * j + k + i] += w_virt[0,j,k]
+        
+        # Delete/Deallocate arrays for memory conservation
+        xv = None
+        yv = None
+        xiv = None
+        w_x = None
+        w_virt = None
     
     return w, w_virt, xv, yv, xiv